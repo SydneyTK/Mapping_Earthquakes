fe986d556a7224a3c8bb3b2e042fbf51993d7b7b
SydneyTK <sydney9797@gmail.com>
2022-02-05 18:45:18 -0500

    completed 13.4.2

---
+++ Mapping_Multiple_Points/static/js/cities.js
@@ -0,0 +1,35 @@
+var cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
+
+  // 13.4.2 Below is the array that contains the 5 most populos cities
+// now we will have our plotting/marker variable iterte through this array 
+// An array containing each city's location, state, and population.
