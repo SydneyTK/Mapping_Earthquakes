f613193cca1fc66a92a0e1a179acc5b46a9aad48
SydneyTK <sydney9797@gmail.com>
2022-02-05 11:38:54 -0500

    forgot one thing in Logic.js, added old examples

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -3,6 +3,14 @@ console.log("working");
 // 13.2.4 Create the map object with a center and zoom level 
 let map = L.map('mapid').setView([34.0522,-118.2437],14);
 
+// 13.4.1 Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//add a circle marker
+//let marker =L.circle([34.0522,-118.2437],{
+    //radius: 100
+//}).addTo(map);
+
 // add a yellow circle marker 
 let marker= L.circleMarker([34.0522,-118.2437],{
     radius: 300,
@@ -11,7 +19,7 @@ let marker= L.circleMarker([34.0522,-118.2437],{
 }).addTo(map);
 
 // 13.2.4 We create the tile layer that will be the background of our map.
-// We create the tile layer that will be the background of our map.
+// We create the tile layer that will be the background of our map. this link is the dark map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
